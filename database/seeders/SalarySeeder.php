0232df01e7e447236f588d112a76a27718df6d0f
Lucas Kelim Thiel <lucas.kthiel@gmail.com>
2025-02-27 17:54:18 -0300

    feat: added Salary Seeder

--- database/seeders/SalarySeeder.php
+++ database/seeders/SalarySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Salary;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class SalarySeeder extends Seeder
      */
     public function run(): void
     {
-        
+        Salary::factory()->count(10)->create();
     }
 }
