6c505a748165a92c3db546a1878b806fc7170803
Lucas Kelim Thiel <lucas.kthiel@gmail.com>
2025-03-04 19:20:47 -0300

    feat: added totalExpenses and remainungSalary methods

--- resources/js/types/Salary.ts
+++ resources/js/types/Salary.ts
@@ -1,9 +1,14 @@
+import { User } from ".";
 import { Expense } from "./Expense";
 
-export interface Salary {
+export interface Salary
+{
     id: number;
-    value: string;
+    value: number | string;
     user_id: number;
     created_at: Date;
     expenses: Expense[];
+    total_expenses: number;
+    remaining_salary: number;
+    user: User;
 }
